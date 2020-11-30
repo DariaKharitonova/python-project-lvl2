e1ff23f9ce2d1cf9c965098d8637f3340da055a8
Daria <kharitonovadari@gmail.com>
2020-11-30 19:57:57 +0300

    + recursive diff

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,4 +1,3 @@
-
 def generate_diff(file1, file2):
     keys_1, keys_2 = set(file1.keys()), set(file2.keys())
     deleted_keys = keys_1 - keys_2
@@ -27,6 +26,13 @@ def generate_diff(file1, file2):
                 'value': file1[key],
                 'status': ' '
             })
+        elif type(file1[key]) is dict and type(file2[key]) is dict and file1[key] != file2[key]:
+            res = generate_diff(file1[key], file2[key])
+            result.append({
+                'key': key,
+                'value': res,
+                'status': ' '
+            })
         else:
             result.append({
                 'key': key,
@@ -39,12 +45,4 @@ def generate_diff(file1, file2):
                 'status': '+'
             })
 
-    result_string = '{\n'
-    for x in result:  # 3
-        result_string += f'  {x["status"]} {x["key"]}: {x["value"]}\n'
-    result_string += '}'
-    result_string = result_string\
-        .replace("True", "true")\
-        .replace("False", "false")\
-        .replace("None", "null")
-    return result_string
+    return result
--- gendiff/scripts/main.py
+++ gendiff/scripts/main.py
@@ -1,21 +1,28 @@
 import argparse
 from gendiff.get_data.get_data import get_data
 from gendiff.gendiff import generate_diff
+from gendiff.formatter import formatter
 
 
 def main():
     parser = argparse.ArgumentParser(description='Generate diff')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    parser.add_argument('-f', '--format', help='set format of output')
+    parser.add_argument('-f', '--format', help='set format of output', default='stylish')
     args = parser.parse_args()
     first_file = args.first_file
     second_file = args.second_file
+    format = args.format
 
     file1 = get_data(first_file)
     file2 = get_data(second_file)
 
-    print(generate_diff(file1, file2))
+    result = generate_diff(file1, file2)
+
+    if format == 'stylish':
+        print(formatter(result))
+    else:
+        print("Wrong format")
 
 
 if __name__ == '__main__':
