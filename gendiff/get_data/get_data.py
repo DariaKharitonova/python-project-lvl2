8b20462fa1efef029cb6acaed8593f1e0075832f
Daria <kharitonovadari@gmail.com>
2020-12-14 22:04:03 +0300

    try fix hexlet-check errors

--- gendiff/get_data/get_data.py
+++ gendiff/get_data/get_data.py
@@ -1,15 +1,12 @@
 import json
 import yaml
 
-JSON = 'json'
-YAML = 'yaml'
-
 
 def get_data(file_path):
     file_extension = file_path.split(".")[1]
 
     with open(file_path, 'r') as file:
-        if file_extension == JSON:
+        if file_extension == 'json':
             return json.load(file)
-        elif file_extension == YAML:
+        elif file_extension == 'yaml' or file_extension == 'yml':
             return yaml.load(file, Loader=yaml.FullLoader)
