af1d836790ccdffb2ebaf40dbbd1b2aff1c3c86d
Daria <kharitonovadari@gmail.com>
2020-12-14 11:17:40 +0300

    try fix hexlet-check errors

--- gendiff/tests/test_gendiff_simple.py
+++ gendiff/tests/test_gendiff_simple.py
@@ -1,4 +1,4 @@
-from gendiff.generate_diff import generate_diff
+from gendiff.gendiff import generate_diff
 from gendiff.formatters.stylish import stylish
 from gendiff.formatters.plain import plain
 from gendiff.formatters.json import get_json_format
