0c938486bb64fe352b6f42a38d49350ac11ad835
Daria <kharitonovadari@gmail.com>
2020-12-14 21:05:01 +0300

    try fix hexlet-check errors

--- gendiff/get_tree_diff.py
+++ gendiff/get_tree_diff.py
@@ -15,21 +15,21 @@ def get_diff(data1, data2):
         if key in deleted_keys:
             result.append({
                 'key': key,
-                'value': generate_diff(data1[key], data1[key])
+                'value': get_diff(data1[key], data1[key])
                 if isinstance(data1[key], dict) else data1[key],
                 'status': REMOVED
             })
         elif key in add_keys:
             result.append({
                 'key': key,
-                'value': generate_diff(data2[key], data2[key])
+                'value': get_diff(data2[key], data2[key])
                 if isinstance(data2[key], dict) else data2[key],
                 'status': ADDED
             })
         elif data1[key] == data2[key]:
             result.append({
                 'key': key,
-                'value': generate_diff(data1[key], data2[key])
+                'value': get_diff(data1[key], data2[key])
                 if isinstance(data1[key], dict) else data1[key],
                 'status': UNCHANGED
             })
@@ -37,16 +37,16 @@ def get_diff(data1, data2):
             if isinstance(data1[key], dict) and isinstance(data2[key], dict):
                 result.append({
                     'key': key,
-                    'value': generate_diff(data1[key], data2[key]),
+                    'value': get_diff(data1[key], data2[key]),
                     'status': UNCHANGED
                 })
             else:
                 result.append({
                     'key': key,
                     'value': {
-                        'old': generate_diff(data1[key], data1[key])
+                        'old': get_diff(data1[key], data1[key])
                         if isinstance(data1[key], dict) else data1[key],
-                        'new': generate_diff(data2[key], data2[key])
+                        'new': get_diff(data2[key], data2[key])
                         if isinstance(data2[key], dict) else data2[key],
                     },
                     'status': UPDATED
