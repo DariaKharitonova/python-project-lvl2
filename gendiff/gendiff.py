cc45db31657af6ac42c47d01b1b681a628f11926
Daria <kharitonovadari@gmail.com>
2020-12-14 23:34:39 +0300

    try fix hexlet-check errors

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -6,9 +6,5 @@ from gendiff.formatters.formater import view_format, DEFAULT_STYLE
 def generate_diff(first_file_path, second_file_path, format=DEFAULT_STYLE):
     first_file = get_data(first_file_path) or {}
     second_file = get_data(second_file_path) or {}
-    print('###FIRST_FILE')
-    print(first_file)
-    print('###SECOND_FILE')
-    print(second_file)
     result_diff = get_diff(first_file, second_file)
     return view_format(result_diff, format)
