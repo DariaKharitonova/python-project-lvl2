d1dc5cc6a35fc25f1f6844fb637d14c3f49efba4
Daria <kharitonovadari@gmail.com>
2020-12-15 12:17:02 +0300

    try fix hexlet-check errors

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -42,29 +42,18 @@ def format_updated(data, indent):
     old_value = data["value"]["old"]
     new_value = data["value"]["new"]
     if isinstance(old_value, list):
-        result += format_nested('', {"key": data["key"], "value": old_value, "status": "removed"}, indent)
+        result += format_nested('', {"key": data["key"],
+                                     "value": old_value,
+                                     "status": "removed"}, indent)
     else:
         result += f'{spaces}- {data["key"]}: {old_value}\n'
     if isinstance(new_value, list):
-        result += format_nested('', {"key": data["key"], "value": new_value, "status": "added"}, indent)
+        result += format_nested('', {"key": data["key"],
+                                     "value": new_value,
+                                     "status": "added"}, indent)
     else:
         result += f'{spaces}+ {data["key"]}: {new_value}\n'
 
-    return result
-
-
-def get_format(x, indent=2):
-    result = ""
-    spaces = " " * indent
-
-    if x['status'] == 'updated':
-        return format_updated(x, indent)
-
-    if type(x['value']) is list:
-        sort = x['status'] != 'added'
-        return format_nested(result, x, indent, sort)
-
-    result += f'{spaces}{STATUSES[x["status"]]} {x["key"]}: {x["value"]}\n'
     return result
 
 
