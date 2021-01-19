4b716a5577e2c98165cb224c2327e3872eb6ad46
Daria <kharitonovadari@gmail.com>
2021-01-19 21:09:20 +0300

    fixed errors on the 2nd review

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -1,7 +1,6 @@
 from gendiff.get_tree_diff import ADDED, REMOVED, UPDATED, UNCHANGED
 from gendiff.helpers.format import format_json_values
 
-
 STATUSES = {
     ADDED: '+',
     REMOVED: '-',
@@ -17,80 +16,59 @@ def stylish(result, indent=2):
     return result_string
 
 
+def form_dict_value(data, indent=2):
+    spaces = get_spaces(indent)
+    return f'{{\n{data}{spaces}  }}'
+
+
 def format_nested(x, indent=2):
-    spaces = get_spaces(indent)
-    indent += 4
-    data_string = ''.join(get_format(y, indent) for y in x['value'])
-    result = f'{spaces}{STATUSES[x["status"]]} ' \
-             f'{x["key"]}: {{\n{data_string}{spaces}  }}\n'
+    data_string = ''.join(get_format(y, indent + 4) for y in x['value'])
+    result = format_string(x['status'], x['key'],
+                           form_dict_value(data_string, indent), indent)
     return result
-#
-#
-# def format_updated(data, indent):
-#     result = ''
-#     spaces = get_spaces(indent)
-#     old_value = data["value"]["old"]
-#     new_value = data["value"]["new"]
-#     if isinstance(old_value, list) and len(old_value) != 0 \
-#             and isinstance(old_value[0], dict):
-#         result += format_nested({"key": data["key"],
-#                                  "value": old_value,
-#                                  "status": REMOVED}, indent)
-#     else:
-#         result += f'{spaces}- {data["key"]}: {format_json_values(old_value)}\n'
-#
-#     if isinstance(new_value, list) and len(new_value) != 0 \
-#             and isinstance(new_value[0], dict):
-#         result += format_nested({"key": data["key"],
-#                                  "value": new_value,
-#                                  "status": ADDED}, indent)
-#     else:
-#         result += f'{spaces}+ {data["key"]}: {format_json_values(new_value)}\n'
-#
-#     return result
 
 
-def f(key, value, indent=2):
-    spaces = get_spaces(indent)
-    result = ''
-    result += f'{spaces}{key}: {{\n'
-    for k, v in value.items():
-        if isinstance(v, dict):
-            result += f(k, v, indent+4)
-        else:
-            result += f'{get_spaces(indent + 4)}{k}: {v}\n'
-    result += f'{spaces}}}\n'
-    return result
+def format_inner_properties(key, value, indent=2):
+    if isinstance(value, dict):
+        return format_dict({"key": key,
+                            "value": value,
+                            "status": UNCHANGED}, indent + 4)
+
+    return format_string(UNCHANGED, key, value, indent + 4)
 
 
 def format_dict(x, indent=2):
-    spaces = get_spaces(indent)
-    indent += 6
-    inner_spaces = get_spaces(indent)
-    result = f'{spaces}{STATUSES[x["status"]]} {x["key"]}: {{\n'
-    for key, value in x['value'].items():
-        if isinstance(value, dict):
-            result += f(key, value, indent)
-        else:
-            result += f'{inner_spaces}{key}: {value}\n'
-
-    result += f"{spaces}  }}\n"
+    data_string = "".join(format_inner_properties(key, value, indent)
+                          for key, value in x['value'].items())
+    result = format_string(x["status"], x['key'],
+                           form_dict_value(data_string, indent), indent)
     return result
+
+
+def form_dict(key, value, status):
+    return {"key": key, "value": value, "status": status}
 
 
 def format_updated(data, indent):
-    spaces = get_spaces(indent)
-    old_value = format_dict({"key": data["key"], "value": data["value"]["old"], "status": REMOVED}, indent) if isinstance(data["value"]["old"], dict) else f'{spaces}- {data["key"]}: {format_json_values(data["value"]["old"])}\n'
-    new_value = format_dict({"key": data["key"], "value": data["value"]["new"], "status": ADDED}, indent) if isinstance(data["value"]["new"], dict) else f'{spaces}+ {data["key"]}: {format_json_values(data["value"]["new"])}\n'
-    # print(old_value, new_value)
-    result = f'{old_value}{new_value}'
-    # print(result)
+    old_value = data["value"]["old"]
+    new_value = data["value"]["new"]
+    old_value_string = format_dict(form_dict(data["key"], old_value, REMOVED), indent) \
+        if isinstance(old_value, dict) else format_string(REMOVED, data["key"], old_value, indent)  # noqa: E501
+    new_value_string = format_dict(form_dict(data["key"], new_value, ADDED), indent) \
+        if isinstance(new_value, dict) else format_string(ADDED, data["key"], new_value, indent)  # noqa: E501
+
+    result = f'{old_value_string}{new_value_string}'
     return result
 
 
-def get_format(x, indent=2):
+def format_string(status, key, value, indent=2):
     spaces = get_spaces(indent)
+    status_symbol = STATUSES[status]
 
+    return f'{spaces}{status_symbol} {key}: {format_json_values(value)}\n'
+
+
+def get_format(x, indent=2):
     if x['status'] == UPDATED:
         return format_updated(x, indent)
 
@@ -100,8 +78,8 @@ def get_format(x, indent=2):
     if isinstance(x["value"], dict):
         return format_dict(x, indent)
 
-    result = f'{spaces}{STATUSES[x["status"]]} ' \
-             f'{x["key"]}: {format_json_values(x["value"])}\n'
+    result = format_string(x['status'], x['key'], x['value'], indent)
+
     return result
 
 
